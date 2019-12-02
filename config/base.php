17ab73c826aaed5d6451d581911d181254bc8b49
Co_LF <1839033430@qq.com>
2019-12-02 08:50:09 +0800

    更新秘钥文件

--- config/base.php
+++ config/base.php
@@ -1,6 +1,6 @@
 <?php return array (
   'site_name' => 'OneIndex',
-  'password' => 'oneindex',
+  'password' => '1122',
   'style' => 'material',
   'onedrive_root' => '',
   'cache_type' => 'secache',
@@ -88,7 +88,7 @@
   'images' => 
   array (
     'home' => false,
-    'public' => true,
+    'public' => false,
     'exts' => 
     array (
       0 => 'jpg',
@@ -97,8 +97,8 @@
       3 => 'bmp',
     ),
   ),
-  'client_secret' => 'eekePOWIM13487[hzzGC[}$',
-  'client_id' => 'ad934aa1-3198-4407-98ea-b7dbc4cb0186',
+  'client_secret' => 'avBCGJXK459@-*zhrgmX04~',
+  'client_id' => '5d9e1c44-15fc-4197-bb7b-e1b47d43fd56',
   'redirect_uri' => 'https://oneindex.github.io/',
-  'refresh_token' => 'OAQABAAAAAAAP0wLlqdLVToOpA4kwzSnxi4O7a5_T1CfeCJnOpZKdTxLR8ojZ8dsRaUbsAMTG_0XVZdheqdSe-ihfb_CbPWu2QoeWUMvXHLuN8gl3fBcZxaVuGCKgJo3rD7Po2IuzUo8PcKR332LuXTIM0ao61ExpFXVw135zo483HEWMYMVEU0gA51gOLVzk5LROIm0ky-ru7oH83gFpz4Z-xH4pNUdemhByFGR2g67-IHqb4RTbPVPgh0nX-pPWaZu2gThYtmTc1Tj0ukNMhts_IRUfSS1TI9uTxqUh8DFT0WBayqeyUulr3s31nCMU0YziAQNUvQBU9lzelXSypyj_EK8hb9M9tWkvSoCtfziqsltch1GkrEGIGrTl3eaFOpUMku7grH5c7bx2tZGJw4kNbGbWQILJWHPLKY4SnPQiOgiORQq_Y0msMKbzkOb_FJeW2cj1B54-p7RVMxFU_yewNN8ks4o-ZBl9XoftqGOy_RUxaQUnA2T62n5wu5EzMOBaOJvkSFbDnespi4fDGEYrMziEfjC05L8pcldn2LjkkZ0VSLKtmtutDCm3CxX3h1JI8xkHSe3E4L0fR1f62IBKHC-GgAjT_8SIynHT21mEyb-EgqsdpA-SZd3kJYtEwhKnojToP0SaEdUufO5jmLFMJT7irul3lnlkBtKDMrh9ZdU8lDTn_tUcUnxBNFMdJtDAP-wnnxmCLjO-kA_Kf2737veQRK1HWm2L21pmkaRf6fVtBoHnA047umy26ytO2fyQ1lAfVUjTakYzum7710mVBDZYFQ6i0Sa0yaM7Zsu49BYTw_AW1TQfyNXIV9wsSUtf0mkmBk8kq42ZGERCs0Vejo5F8iMHR7EfI8kUod2Qj1xFrKAdT3kb76MYRADS45SO9Lk2YRlrhKW24_aa6gmw4jeqIbspG1gi_A0c5qJcpKxU_OxFI646yB3qT5hFzAMYYtz3TLJco0-g0RaX4agMP93L8hUwIAA',
+  'refresh_token' => 'OAQABAAAAAACQN9QBRU3jT6bcBQLZNUj7SmBYanpT6NUTVNr43b2Q2gDCiv3Z5LEv2QLYhwX_mQvwozHtyaP-FvXhBZbDDIguuc0rtcgUO9gWgzjzSpJjgvbk5aPHeWuaMvGnj9N7yGKWhFBpG0v0V4wNemyrI2wxJFhnOt_snvdn_xbw1wBXcPeFxaPC8o9GRHHgIaB__-4AxfYgdsXWfxBEGNP2gU1aapSTpq1zd4IKsGR6KyVsCjEV_KSxX5T0sXlnhKmZxadkGMVs_ZgNLM1EKZ9PXn_gGXlHpMOoCn2mK-o52qvMfddn5lxDjhGTwdC7lyz0Q3fwvQiJxJOGIJUcqZJG2Gpk4B5pGKfudCjyAyD8qMcdookoefZRbWheObCZCjlckVhIOlFxxjeaqY15lFM7EuwvbSaHpYBXc9xuqHPN_gLtIKlefOJ250NfX2NrVjUXdd1rNsYeMsJjlSso53MH9mRY8w9EMqgZNzHezEI-sxEcL167AGPRI9QMLqI-uSc8Fynvv5G87r9P1NvM4xg9y10EdgEOphqv2RtxeUaCkrM79lNShmpOOvWCZpmMMGNhGE_42_XAZuVgOxHjsNmXRn0yZ-_N3HXu-owM67UPGsORkhH3ZaFit4qtQOE5yGrwhoNCbVVpWpq7e0uCBAXS0uMtPiH8cwgvoxN5lX3fb3hDeUQAHvOspY4Q-xi6BK5TUaeXR9ark88j9ju8GZdpLNYMln6tXBHLiSiPi13fm6UJ3Vxha30icqOMs2fbQ1pSQYtP5FDNwnk3n6KwLz2n1Z2bRX-vcDVn1CuSt1OYtQKXl9Z9hm2IeDN7Z929KzPrZX0gAA',
 );
